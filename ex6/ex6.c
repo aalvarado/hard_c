908f14e4c99bf7178e0e25799a35ab862443a446
Adan Alvarado <adan.alvarado7@gmail.com>
2013-05-10 16:54:59 -0700

    Add exercise 6

---
+++ ex6/ex6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int distance = 100;
+	float power = 2.345f;
+	double super_power = 56789.4532;
+	char initial = 'A';
+	char first_name[] = "Adan";
+	char last_name[] = "Alvarado";
+
+	printf("Your are %d miles away \n", distance);
+	printf("you have %f levels of power\n", power);
+	printf("you have %f awesome super powers\n", super_power);
+	printf("I have an initial %c\n", initial);
+	printf("I have a first name %s\n", first_name);
+	printf("I have a last name %s \n", last_name);
+	printf("My whole name is %s %c. %s\n", first_name, initial, last_name);
+	return 0;
+}
