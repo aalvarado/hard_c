39dc69d0696864659e1325bb053a36b714562e03
Adan Alvarado <adan.alvarado7@gmail.com>
2013-05-10 15:37:42 -0700

    Add exercise 4

---
+++ ex4/ex4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* this program is wrong on purpose */
+
+int main()
+{
+	int age = 10;
+	int height;
+
+	printf("I am %d years old\n");
+	printf("I am %d inches old\n", height);
+
+	return 0;
+}
