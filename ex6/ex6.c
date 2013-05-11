9b796973d30dac7bd1246c7b1db188abea72498a
Adan Alvarado <adan.alvarado7@gmail.com>
2013-05-10 17:00:16 -0700

    Broken ex6

--- ex6/ex6.c
+++ ex6/ex6.c
@@ -15,6 +15,9 @@ int main(int argc, char const *argv[])
 	printf("I have an initial %c\n", initial);
 	printf("I have a first name %s\n", first_name);
 	printf("I have a last name %s \n", last_name);
-	printf("My whole name is %s %c. %s\n", first_name, initial, last_name);
+
+	// breaking it
+	/*printf("My whole name is %s %c. %s\n", first_name, initial, last_name);*/
+	printf("My whole name is %s %c. %s\n", initial, first_name,  last_name);
 	return 0;
 }
