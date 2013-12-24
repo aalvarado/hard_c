ce5f28b1449e400b6998c6fc2cc7e4a09a51b844
aalvarado <adan.alvarado7@gmail.com>
2013-12-23 20:17:27 -0800

    ex8 update

--- ex8/ex8.c
+++ ex8/ex8.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
+	char full_name[] = {
+		'A', 'd', 'a','n',
+		' ', 'A', 'l','v',
+		'a','r','a','d','o'
+		/*,'\0'*/
+	};
+
+
 int main(int argc, char const *argv[])
 {
 	int areas[] = {10,12,13,14,20};
 	char name[] = "Zed";
-	char full_name[] = {
-		'A', 'd', 'a','n',
-		' ', 'A', 'l','v',
-		'a','r','a','d','o',
-		'\0'
-	};
-
 	printf("The size of an int: %ld\n", sizeof(int));
 	printf("the size of areas (int[]): %ld\n", sizeof(areas));
 	printf("the number of ints in areas %ld\n", sizeof(areas) / sizeof(int));
-	printf("the first area is %d the 2nd %d\n", areas[0], areas[1]);
+	printf("the first area is %d the 2nd %d\n", areas[10], areas[1]);
 	printf("the size of a char %ld\n", sizeof(char));
 	printf("the size of name (char[]): %ld\n", sizeof(name));
 	printf("the number of chars: %ld\n",sizeof(name) / sizeof(char));
