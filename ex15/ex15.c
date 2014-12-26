4ff48638110abd7eb46310c209b8f15e6bf46d91
aalvarado <adan.alvarado7@gmail.com>
2014-12-25 23:01:03 -0800

    Update ex 15 with complete example

--- ex15/ex15.c
+++ ex15/ex15.c
@@ -14,5 +14,25 @@ int main(int argc, char const *argv[]) {
 		printf("%s has %d years alive\n",
 			names[i], ages[i]);
 	}
+
+	printf("-------\n");
+
+	// setup the pointer to the start of the arrays
+	int *cur_age = ages;
+	char **cur_name = names;
+
+	for (i = 0; i < count; ++i) {
+		printf("%s is %d years old\n", 
+			*(cur_name+i), *(cur_age+i));
+	}
+
+	printf("-------\n");
+
+	for (cur_name = names, cur_age = ages;
+		( cur_age - ages) < count; 
+		++cur_name, ++cur_age) {
+		printf("%s lived %d so far.\n", *cur_name, *cur_age);
+	}
+
 	return 0;
 }
